f36ec6fb815c2221b0dbe4639864a3f28377dc08
ValdezR <valdez.ramonh@gmail.com>
2022-05-24 15:53:23 -0300

    commit

---
+++ Myapp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.WriteLine("Ingrese un numero: ");
+string numero=Console.ReadLine();
+char [] array = numero.ToCharArray();
+
+Console.WriteLine("El numero ingresado es: " + numero);
+Console.Write("El numero invertido es: ");
+
+for (int i = 1; i <= array.Length; i++)
+{
+    Console.Write(array[array.Length-i]);
+}
+
