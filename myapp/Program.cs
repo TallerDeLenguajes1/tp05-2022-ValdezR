b3db71b485db24c8891586790a401dfcfb81d7be
ValdezR <valdez.ramonh@gmail.com>
2022-05-31 16:43:02 -0300

    commit

--- myapp/Program.cs
+++ myapp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.WriteLine("Ingrese un numero: ");
+string numero=Console.ReadLine();
+char [] array = numero.ToCharArray();
+
+Console.WriteLine("El numero ingresado es: " + numero);
+Console.Write("El numero invertido es: ");
+
+for (int i = 1; i <= array.Length; i++)
+{
+    Console.Write(array[array.Length-i]);
+}
