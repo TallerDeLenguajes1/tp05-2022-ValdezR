b1f3e0df80095fbcd46886cad193ac909c05f27e
ValdezR <valdez.ramonh@gmail.com>
2022-06-02 01:22:33 -0300

    commit

--- myapp/Program.cs
+++ myapp/Program.cs
@@ -1,46 +1,46 @@
 ﻿using System;
 
-namespace ejercicio2
+namespace ejercicio3
 {
     class Program
     {
         static void Main ()
         {
-            int operacion = 5;
-            do
+            float numero1 = 0;
+            int numero2 = 0;
+            int numero3 = 0;
+
+            Console.Clear();
+            Console.Write("Ingrese un numero: ");
+            numero1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine($"El numero ingresado es: {numero1}");
+            Console.WriteLine($"Su valor Absoluto es: " + Math.Abs(numero1));
+            Console.WriteLine($"El cuadrado del numero es: " + Math.Pow(numero1, 2));
+            Console.WriteLine($"Su raiz cuadrada es: " + Math.Sqrt(numero1));
+            Console.WriteLine($"El seno es: " + Math.Sin(numero1));
+            Console.WriteLine($"El coseno es: " + Math.Cos(numero1));
+            Console.WriteLine($"La parte entera del numero es: " + Math.Truncate(numero1));
+
+            Console.WriteLine("\n\n--Ingrese 2 numeros--\n\n");
+            Console.Write("Ingrese el primero: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese el segundo: ");
+            numero3 = Convert.ToInt32(Console.ReadLine());
+            if (numero2>numero3)
             {
-                int resultado = 0;
-
-                Console.Clear();
-                Console.WriteLine("Ingrese la operacion que desea realizar:\n1. SUMA\n2. RESTA\n3. MULTIPLICACION\n4. DIVISION\n0. SALIR");
-                operacion = Convert.ToInt32(Console.ReadLine());
-
-                if (operacion == 1 || operacion == 2 || operacion == 3 || operacion == 4)
+                Console.WriteLine($"el numero {numero2} es el maximo ingresado");
+                Console.WriteLine($"el numero {numero3} es el menor ingresado");
+            }else
+            {
+                if (numero3>numero2)
                 {
-                    Console.Write("Ingrese el primer numero: ");
-                    int numero1 = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Ingrese el segundo numero: ");
-                    int numero2 = Convert.ToInt32(Console.ReadLine());
-                    if (operacion == 1)
-                    {
-                        resultado = numero1 + numero2;
-                    }
-                    if (operacion == 2)
-                    {
-                        resultado = numero1 - numero2;
-                    }
-                    if (operacion == 3)
-                    {
-                        resultado = numero1 * numero2;
-                    }
-                    if (operacion == 4 && numero2 != 0)
-                    {
-                        resultado = numero1 / numero2;
-                    }
-                    Console.WriteLine("--");
-                    Console.WriteLine("El resultado es: " + resultado);
+                    Console.WriteLine($"el numero {numero3} es el maximo ingresado");
+                    Console.WriteLine($"el numero {numero2} es el menor ingresado");
+                }else
+                {
+                    Console.WriteLine("Los numeros ingresados son iguales");
                 }
-            } while (operacion != 0) ;
+            }
         }
     }
 }
