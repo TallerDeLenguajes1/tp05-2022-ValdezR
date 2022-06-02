a6e1c65e0fe7f4e2361c348bcc307aab4503a104
ValdezR <valdez.ramonh@gmail.com>
2022-06-02 00:59:04 -0300

    commit

--- myapp/Program.cs
+++ myapp/Program.cs
@@ -12,47 +12,33 @@ namespace ejercicio2
                 int resultado = 0;
 
                 Console.Clear();
-                Console.WriteLine("1. SUMA");
-                Console.WriteLine("2. RESTA");
-                Console.WriteLine("3. MULTIPLICACION");
-                Console.WriteLine("4. DIVISION");
-                Console.WriteLine("0. SALIR");
-                Console.WriteLine("--");
-                Console.Write("Ingrese la opcion: ");
-                operacion = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la operacion que desea realizar:\n1. SUMA\n2. RESTA\n3. MULTIPLICACION\n4. DIVISION\n0. SALIR");
+                operacion = Convert.ToInt32(Console.ReadLine());
 
                 if (operacion == 1 || operacion == 2 || operacion == 3 || operacion == 4)
                 {
                     Console.Write("Ingrese el primer numero: ");
-                    int numero1 = int.Parse(Console.ReadLine());
+                    int numero1 = Convert.ToInt32(Console.ReadLine());
                     Console.Write("Ingrese el segundo numero: ");
-                    int numero2 = int.Parse(Console.ReadLine());
-
+                    int numero2 = Convert.ToInt32(Console.ReadLine());
                     if (operacion == 1)
                     {
                         resultado = numero1 + numero2;
-
                     }
                     if (operacion == 2)
                     {
                         resultado = numero1 - numero2;
-
                     }
                     if (operacion == 3)
                     {
                         resultado = numero1 * numero2;
-
                     }
                     if (operacion == 4 && numero2 != 0)
                     {
                         resultado = numero1 / numero2;
-
                     }
                     Console.WriteLine("--");
                     Console.WriteLine("El resultado es: " + resultado);
-                    Console.Write("Presione una tecla...");
-                    Console.Read();
-
                 }
             } while (operacion != 0) ;
         }
